dd7e07c4ff00886096ddae909c87748985ea39e6
artiphox <josemgnc@gmail.com>
2021-12-05 22:19:31 +0100

    start

---
+++ server.js
@@ -0,0 +1,35 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {
+    temp: 0,
+    feel: 0,
+    date: 0,
+};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+//Callback to debug
+function listening(){
+    console.log("server running");
+    console.log(`running on localhost: ${port}`);
+}
